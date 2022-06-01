efcdff20a0b4dcc8d45f405779e59838e7693ba3
DanGreenhaus <88389380+DanGreenhaus@users.noreply.github.com>
2022-06-01 15:40:31 -0400

    Create FindAppVersion.ps1

---
+++ FindAppVersion.ps1
@@ -0,0 +1,13 @@
+ForEach($server in $servers) {
+  Write-Host "processing workstation $server"
+  invoke-command -ComputerName $server -ScriptBlock {
+    $InstalledSoftware = Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall"
+    foreach($obj in $InstalledSoftware){
+        if ($obj.GetValue('DisplayName') -like "*Chrome*") {write-host $obj.GetValue('DisplayName') -NoNewline; write-host " - " -NoNewline; write-host $obj.GetValue('DisplayVersion')}
+    }
+    $wow6432=Get-ChildItem "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+    foreach($obj in $wow6432){
+        if ($obj.GetValue('DisplayName') -like "*Chrome*") {write-host $obj.GetValue('DisplayName') -NoNewline; write-host " - " -NoNewline; write-host $obj.GetValue('DisplayVersion')}
+    }
+  }
+}
